dcd651818e5cff94ef26b503b34f208dccc755eb
Diego Almeida <diegoalmda@gmail.com>
2022-10-04 16:29:06 -0300

    feat: helper scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 import { Router } from './Router'
 import { GlobalLanguageContextProvider } from './contexts/GlobalLanguageContext'
+import { ScrollToTheTop } from './helpers/ScrollToTheTop'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <GlobalLanguageContextProvider>
+          <ScrollToTheTop />
           <Router />
         </GlobalLanguageContextProvider>
       </BrowserRouter>
